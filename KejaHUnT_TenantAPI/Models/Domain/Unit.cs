5b070fd8eaf4b6d51e7ef3d76b7ee5f0b24de986
Karol Kiptoo <Karol Kiptoo@KAROL-KIPTOO>
2025-04-19 15:02:21 +0300

    vv

---
+++ KejaHUnT_TenantAPI/Models/Domain/Unit.cs
@@ -0,0 +1,14 @@
+﻿namespace KejaHUnT_TenantAPI.Models.Domain
+{
+    public class Unit
+    {
+        public long Id { get; set; }
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+        public int Bathrooms { get; set; }
+        public double Size { get; set; }
+        public int NoOfUnits { get; set; }
+        public int PropertyId { get; set; }
+        public Tenant Tenant { get; set; }
+    }
+}
